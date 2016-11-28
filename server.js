927697c37f97b81ea97b0838d8d2620bb354bf9f
mnfmnfm <mnfmnfm@gmail.com>
2016-11-27 16:11:43 -0800

    Update server.js

--- server.js
+++ server.js
@@ -8,6 +8,8 @@ app.use(express.static(path.join(__dirname, '/public')));
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json());
 
+app.set('port', (process.env.PORT || 5000));
+
 app.get('/', express.static(path.join(__dirname, 'public')));
 
 app.get('/favorites', function(req, res){
@@ -17,23 +19,27 @@ app.get('/favorites', function(req, res){
 });
 
 app.post('/favorites', function(req, res){
-  
   if(!req.body.Title || !req.body.imdbID){
-  	var response = "empty";
-  	for (var item in req.body) {
-  		response += item + ":" + body[item] + ",";
-  	}
-    res.send("Error: Missing body.Title or body.imdbID in:" + response);
+    res.send("Error: Missing body.Title or body.imdbID");
     return;
   }
   
-  var data = JSON.parse(fs.readFileSync('./data.json'));
-  data.push(req.body);
-  fs.writeFile('./data.json', JSON.stringify(data));
+  var fileContents = fs.readFileSync('./data.json');
+  var data = JSON.parse(fileContents);
+  if (!fileContents.includes(req.body.imdbID)) {
+  	data.push(req.body);
+  	fs.writeFile('./data.json', JSON.stringify(data));
+  }
   res.setHeader('Content-Type', 'application/json');
   res.send(data);
 });
 
-app.listen(3000, function(){
-  console.log("Listening on port 3000");
+app.post('/favorites-clear', function (req,res) {
+	fs.truncate('./data.json');
+	fs.writeFile('./data.json', '[]');
+	res.send(null);
+});
+
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
 });
